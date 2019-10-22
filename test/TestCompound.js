5071150c96bc7182af2c34ec020071e6b41518a0
Shain <srslafazan@gmail.com>
2019-10-22 16:26:25 -0700

    In progress

---
+++ test/TestCompound.js
@@ -0,0 +1,25 @@
+// const { time, expectRevert, balance } = require('openzeppelin-test-helpers');
+
+// const toSecs        = require('@mblackmblack/to-seconds');
+// const { sha256 }    = require('@liquality/crypto')
+// const { ensure0x, remove0x   }  = require('@liquality/ethereum-utils');
+// const { BigNumber } = require('bignumber.js');
+// const axios         = require('axios');
+
+// const ExampleCoin = artifacts.require("./ExampleDaiCoin.sol");
+// const ExampleUsdcCoin = artifacts.require("./ExampleUsdcCoin.sol");
+// const USDCInterestRateModel = artifacts.require('./USDCInterestRateModel.sol')
+// const Funds = artifacts.require("./Funds.sol");
+// const Loans = artifacts.require("./Loans.sol");
+// const Sales = artifacts.require("./Sales.sol");
+// const Med = artifacts.require('./MedianizerExample.sol');
+
+// const CErc20 = artifacts.require('./CErc20.sol');
+// const CEther = artifacts.require('./CEther.sol');
+// const Comptroller = artifacts.require('./Comptroller.sol')
+
+// const utils = require('./helpers/Utils.js');
+
+// const { rateToSec, numToBytes32 } = utils;
+// const { toWei, fromWei } = web3.utils;
+
